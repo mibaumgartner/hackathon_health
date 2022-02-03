4f8a686c7749b0df18007134b81ed214debe9106
im2254@partner.kit.edu <im2254@hkn1992.localdomain>
2022-02-03 12:16:05 +0100

    remove install_requires from setup.py

--- setup.py
+++ setup.py
@@ -28,7 +28,6 @@ def read_file(file):
 
 readme = read_file(os.path.join(os.path.dirname(__file__), "README.md"))
 license = read_file(os.path.join(os.path.dirname(__file__), "LICENSE"))
-print(requirements)
 
 setup(
     name="medhack",
