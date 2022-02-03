f907ede977fca396b3c43149ab037591dd907e33
tassilowald <tassilo.wald@dkfz-heidelberg.de>
2022-02-03 13:49:34 +0100

    Updated parser arguments in run_eval.py

--- README.md
+++ README.md
@@ -19,3 +19,10 @@ Inference can be run with the following command:
 ```bash
 sbatch eval_conda.sh --save_dir [dir to save results] --data_dir [directory with csv file and img dir]--ckpt_path [path to checkpoint]
 ```
+
+Details:
+
+1. _save_dir:_ Directory where predictions will be saved to as 'predictions.csv'
+2. _data_dir:_ Directory that contains (a 'test.csv' that contains only an 'image' column;"
+             "The corresponding images are located '{data_dir}/imgs'
+3. _ckpt_path:_ Path to the checkpoint file loaded from Zenodo.
--- scripts/run_eval.py
+++ scripts/run_eval.py
@@ -16,53 +16,20 @@ from pathlib import Path
 from medhack.ptmodule.module import BaseClassificationModule
 
 
-def collect_results_gpu(result_part, size):
-    rank = dist.get_rank()
-    world_size = dist.get_world_size()
-    # dump result part to tensor with pickle
-    part_tensor = torch.tensor(
-        bytearray(pickle.dumps(result_part)), dtype=torch.uint8, device='cuda')
-    # gather all result part tensor shape
-    shape_tensor = torch.tensor(part_tensor.shape, device='cuda')
-    shape_list = [shape_tensor.clone() for _ in range(world_size)]
-    dist.all_gather(shape_list, shape_tensor)
-    # padding result part tensor to max length
-    shape_max = torch.tensor(shape_list).max()
-    part_send = torch.zeros(shape_max, dtype=torch.uint8, device='cuda')
-    part_send[:shape_tensor[0]] = part_tensor
-    part_recv_list = [
-        part_tensor.new_zeros(shape_max) for _ in range(world_size)
-    ]
-    # gather all result part
-    dist.all_gather(part_recv_list, part_send)
-
-    if rank == 0:
-        part_list = []
-        for recv, shape in zip(part_recv_list, shape_list):
-            part_list.append(
-                pickle.loads(recv[:shape[0]].cpu().numpy().tobytes()))
-        # sort the results
-        ordered_results = []
-        for res in zip(*part_list):
-            ordered_results.extend(list(res))
-        # the dataloader may pad some samples
-        ordered_results = ordered_results[:size]
-        return ordered_results
-
-
 class CovidInferenceImageDataset(Dataset):
     def __init__(self, csv_file, root_dir):
         """
+        
         Expects the images to be located in {root_dir}/imgs
+        :param csv_file: Filepath of the CSV file
+        :Param root_dir: Path to the directory containing the data.
         """
         self.test_image_names: List[str] = []
-        with open(os.path.join(root_dir, csv_file), "r") as f:
+        with open(csv_file, "r") as f:
             csv_reader = csv.reader(f)
             next(csv_reader, None)  # Skip header
             for row in csv_reader:
-                self.test_image_names.append(os.path.join(root_dir, "imgs", row[0]))
-        if csv_file == "valid.csv":
-            self.test_image_names = self.test_image_names * int(200000 / len(self.test_image_names))
+                self.test_image_names.append(os.path.join(root_dir, row[0]))
 
         self.transform = Compose([
             Resize((224, 224)),
@@ -87,12 +54,18 @@ if __name__ == "__main__":
         help="Directory where predictions will be saved to as 'predictions.csv'",
         default="/hkfs/work/workspace/scratch/im9193-H1/eval_data",
     )
+    
+    parser.add_argument(
+        "--csv_filepath",
+        type=str,
+        help="Path to the csv file",
+        default="/hkfs/work/workspace/scratch/im9193-H1/eval_data",
+    )
 
     parser.add_argument(
         "--data_dir",
         type=str,
-        help="Directory that contains (a 'test.csv' that contains only 'image' names;"
-             "The corresponding images are located '{data_dir}/imgs')",
+        help="Directory to the folder containing the images of the csv file.)",
         default="/hkfs/work/workspace/scratch/im9193-H1/eval_data",
     )
 
@@ -117,10 +90,13 @@ if __name__ == "__main__":
     save_dir = args.save_dir  # Determines where to save the .csv
     data_dir = args.data_dir  # Decides if test/train is used.
     ckpt_path = args.ckpt_path
+    csv_filepath = args.csv_filepath
+
     # data_dir = os.getcwd()
     os.makedirs(save_dir, exist_ok=True)
 
-    test_run = "test_data" in data_dir
+    test_run = True
+    # "test_data" in data_dir
 
     # load model with pretrained weights
     final_model_arch = "resnet18"
@@ -138,14 +114,9 @@ if __name__ == "__main__":
                                      )
 
     # dataloader
-    print(f"Running inference on {'test' if test_run else 'validation'} data")
+    # print(f"Running inference on {'test' if test_run else 'validation'} data")
 
-    if test_run:
-        # dataset = DummyDataset()
-        dataset = CovidInferenceImageDataset("test.csv", root_dir=data_dir)
-    else:
-        # dataset = DummyDataset()
-        dataset = CovidInferenceImageDataset("valid.csv", root_dir=data_dir)
+    dataset = CovidInferenceImageDataset(csv_filepath, root_dir=data_dir)
 
     trainer = pl.Trainer(
         logger=False,
@@ -230,28 +201,4 @@ if __name__ == "__main__":
             for img_name, prediction in zip(all_image_names, all_preds):
                 csv_writer.writerow([img_name, prediction])
 
-        if test_run == "test":
-            sys.exit()
-        else:
-            print("Done! The result is saved in {}".format(save_dir))
-
-            groundtruths: Dict[str, int] = {}
-            with open(os.path.join(data_dir, "valid_with_labels.csv"), "r") as f:
-                csv_reader = csv.DictReader(f)
-                for row in csv_reader:
-                    groundtruths[row["image"]] = 0 if row["label"] == 'negative' else 1
-
-            read_predictions: Dict[str, int] = {}
-            with open(os.path.join(save_dir, "predictions.csv"), "r") as f:
-                csv_reader = csv.DictReader(f)
-                for row in csv_reader:
-                    read_predictions[row["image"]] = row["prediction"]
-
-            is_true: List[bool] = []
-            for key in groundtruths.keys():
-                gt = groundtruths[key]
-                pd = read_predictions[key]
-                is_true.append(int(gt) == int(pd))
-
-            acc = float(sum(is_true)) / float(len(is_true))
-            print(f"Accuracy read from predictions.csv: {acc}")
+        sys.exit()
